18bf28c9759f4f62ef35fe21668a3285a90aed22
Claire Bohlen <clairebohlen@gmail.com>
2020-12-13 11:44:26 -0700

    JS add class and remove class

--- assests/scripts2.js
+++ assests/scripts2.js
@@ -1,1 +1,18 @@
 const panels2 = document.querySelectorAll('panel2')
+
+//function to add active class
+
+panels2.forEach(panel2) => {
+    panel2.addEventListener('click', ()=>{
+        removeClassList()
+    })
+}
+
+//function to remove class
+
+function removeClassList(){
+    panels2.forEach(panel2)=>{
+        panel2.classList.remove('active2')
+    }
+
+}
