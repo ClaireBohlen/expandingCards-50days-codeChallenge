4645caeb6eb6762204d36578e286f71e96b5a436
Claire Bohlen <clairebohlen@gmail.com>
2020-12-13 14:48:21 -0700

    fixing bug in media query

--- assests/scripts2.js
+++ assests/scripts2.js
@@ -1,18 +1,19 @@
-const panels2 = document.querySelectorAll('panel2')
+const panels2 = document.querySelectorAll('.panel2')
 
 //function to add active class
 
-panels2.forEach(panel2) => {
+panels2.forEach((panel2) => {
     panel2.addEventListener('click', ()=>{
-        removeClassList()
+        removeActiveClass()
+        panel2.classList.add('active2')
     })
-}
+})
 
 //function to remove class
 
-function removeClassList(){
-    panels2.forEach(panel2)=>{
+function removeActiveClass(){
+    panels2.forEach(panel2=>{
         panel2.classList.remove('active2')
-    }
+    })
 
 }
